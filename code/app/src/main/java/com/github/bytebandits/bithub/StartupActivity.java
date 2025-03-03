427af7fb16a8390f6bf0dc1a85cc8868b59ac58b
Hanss <hanss.rivera@gmail.com>
2025-03-03 13:05:29 -0700

    implemented StartupActivity class

--- code/app/src/main/java/com/github/bytebandits/bithub/StartupActivity.java
+++ code/app/src/main/java/com/github/bytebandits/bithub/StartupActivity.java
@@ -1,4 +1,63 @@
 package com.github.bytebandits.bithub;
 
-public class StartupActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+/**
+ * The Startup activity, entry point of the application. Governs anything related to starting up/authenticating your BitHub account.
+ */
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        startupFragment();
+    }
+
+    /**
+     * Initializes the startup fragment
+     */
+    public void startupFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        StartupFragment startupFragment = new StartupFragment();
+        fragmentTransaction.add(R.id.startupFrame, startupFragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Initializes the login fragment, replacing whatever is within the frame layout of activity_startup
+     */
+    public void loginFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        LoginFragment loginFragment = new LoginFragment();
+        fragmentTransaction.replace(R.id.startupFrame, loginFragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Initializes the signup fragment, replacing whatever is within the frame layout of activity_startup
+     */
+    public void signupFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SignupFragment signupFragment = new SignupFragment();
+        fragmentTransaction.replace(R.id.startupFrame, signupFragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Switches activities: from StartupActivity to MainActivity
+     */
+    public void mainActivitySwitch(){
+        Intent intent = new Intent(StartupActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
