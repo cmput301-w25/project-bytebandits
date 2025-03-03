7a4aadf733bb99657b8ea6cda4871b23ffd8394c
Zaid Al-Qadiri <zaidalqadiri@Zaids-MacBook-Air.local>
2025-03-03 16:02:29 -0700

    added bottom navbar and setup the headerbar and the fragments for each page

--- code/app/src/main/java/com/github/bytebandits/bithub/MainActivity.java
+++ code/app/src/main/java/com/github/bytebandits/bithub/MainActivity.java
@@ -7,9 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.github.bytebandits.bithub.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     SessionManager sessionManager;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,40 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.explore) {
+                replaceFragment(new ExploreFragment());
+            } else if (item.getItemId() == R.id.create) {
+                replaceFragment(new CreateFragment());
+            } else if (item.getItemId() == R.id.notifications) {
+                replaceFragment(new NotificationsFragment());
+            } else if (item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
+
+    // Replaces the current fragment with the selected fragment based on the clicked
+    // icon.
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
