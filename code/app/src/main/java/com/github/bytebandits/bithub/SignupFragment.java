ea1c9f30d8ba9ea13209065217dcd062b971da25
Hanss <hanss.rivera@gmail.com>
2025-03-03 13:05:53 -0700

    implemented all StartupActivity fragments

--- code/app/src/main/java/com/github/bytebandits/bithub/SignupFragment.java
+++ code/app/src/main/java/com/github/bytebandits/bithub/SignupFragment.java
@@ -1,4 +1,33 @@
 package com.github.bytebandits.bithub;
 
-public class SignupFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Fragment that displays the signup page
+ */
+public class SignupFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.signup, container, false);
+        Button signup = view.findViewById(R.id.registerBtn);
+        TextView accountExists = view.findViewById(R.id.accountExists);
+        signup.setOnClickListener(v -> {
+            ((StartupActivity) requireActivity()).loginFragment();
+        });
+        accountExists.setOnClickListener(v -> {
+            ((StartupActivity) requireActivity()).loginFragment();
+        });
+
+        return view;
+    }
 }
--- code/app/src/main/java/com/github/bytebandits/bithub/StartupFragment.java
+++ code/app/src/main/java/com/github/bytebandits/bithub/StartupFragment.java
@@ -11,16 +11,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 /**
- * Fragment that displays the login page
+ * Fragment that displays the entry point of the application, can login or register.
  */
-public class LoginFragment extends Fragment {
+public class StartupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.login, container, false);
+        View view = inflater.inflate(R.layout.startup, container, false);
         Button login = view.findViewById(R.id.loginBtn);
+        Button signup = view.findViewById(R.id.registerBtn);
+
         login.setOnClickListener(v -> {
-            ((StartupActivity) requireActivity()).mainActivitySwitch();
+            ((StartupActivity) requireActivity()).loginFragment();
+        });
+
+        signup.setOnClickListener(v -> {
+            ((StartupActivity) requireActivity()).signupFragment();
         });
 
         return view;
