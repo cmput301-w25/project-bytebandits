7dd346597f412113d74a55854bfdc10ec9323d9b
Zaid Al-Qadiri <zaidalqadiri@Zaids-MacBook-Air.local>
2025-03-05 14:58:04 -0700

    created constructors, getters and setters classes for User class

---
+++ code/app/src/main/java/com/github/bytebandits/bithub/User.java
@@ -0,0 +1,50 @@
+package com.github.bytebandits.bithub;
+
+import java.util.UUID;
+
+public class User {
+    UUID uuid;
+    String name;
+    String password;
+    String email = "";
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
