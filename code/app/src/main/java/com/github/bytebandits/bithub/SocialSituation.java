622caf24bdc9b283cdb3df5ee7322d1fda67e332
tony-m-yang <tmyang@ualberta.ca>
2025-03-04 18:06:57 -0700

    Added needed enumerations and mood logos
     - Added Emotions.java which is a enumeration that contains all the moods possible fore a mood post
    - Added SocialSituation.java which is a enumeration that contains all the possible social situations for a mood post
    - Added each mood logo as a xml file under res/drawable

---
+++ code/app/src/main/java/com/github/bytebandits/bithub/SocialSituation.java
@@ -0,0 +1,13 @@
+package com.github.bytebandits.bithub;
+
+/**
+ * This is a enumeration that represents each possible social situation for a mood post
+ * @author Tony Yang
+ */
+
+public enum SocialSituation {
+    ALONE,
+    PARTNER,
+    GROUP,
+    CROWD
+}
