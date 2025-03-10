6392ee7ce68906b44e2a6dbf166db025c92143cc
tony-m-yang <145091491+tony-m-yang@users.noreply.github.com>
2025-03-09 23:12:09 -0600

    Ty/moods - Homepage, Create Post Page, and Mood Post implementation (#80)
    * Added needed enumerations and mood logos
    
     - Added Emotions.java which is a enumeration that contains all the moods possible fore a mood post
    - Added SocialSituation.java which is a enumeration that contains all the possible social situations for a mood post
    - Added each mood logo as a xml file under res/drawable
    
    * Added MoodPost Class
    
    - Haven't added the profile variable yet and it's corresponding spot in the constructor and it's setters and getters
    - Haven't fully implemented the getImage() method
    
    * Added viewing of moods and changed moodpost class
    
    - Created the homepage fragment which currently just displays some initialized data
    - Created adapter for mood posts
    - Changed the representation of images in the moodPost class from a String of a imageURL to just a byte array
    - Added fonts and colours for the project
    
    * Ability to click on mood posts for detailed view
    
    - Added detailedViewFragment
    - Added arrows on top of mood posts to indicate the ability to press them
    - Added method in MoodPost class to return formatted date and time for display
    
    * Added ability to post moods
    
    - Added post mood fragment
    - Adjusted Homepage Fragment to call the post mood fragment on a button press (THIS NEEDS TO BE REMOVED LATER ONCE NAV BAR IS IMPLIMENTED, THIS IS JUST TEMP FOR TESTING)
    
    * Added check for valid desc input
    
    - just edited postmoodfragment slightly so that if the user inputs a description that is more than 20 characters or 3 words for their mood post, they'll get a error and the mood post won't be added
    
    * Edited MoodPost to fit UML
    
    Edited the moodPost class to fit the new UML diagram where image is kept as a BitMap and we don't pass location to the MoodPost but a boolean of whether we want a mood post or not and handle the getting of location internally
    
    * Added ability to edit mood posts
    
    - Edited PostMoodFragment so that if you call a new instance of it with the tag "edit mood post" and pass a mood post to it, you can edit a mood post
    - Edited the setter for location in MoodPost class so that its two methods for enabling and disabling location instead of passing a boolean
    - Added background to the spinners and textbox of the post mood fragment xml so its easier to see the text
    
    * Edited for Profile, Database and delete
    
    - THIS VERSION WILL NOT RUN
    - MoodPost is now had a Profile variable instead of a String variable representing the user
    - Everything is now linked to database class rather than storing locally
    - Got rid of all the features that were put purely for testing
    - Added some javadocs to PostMoodFragment methods
    - Added delete mood post function
    
    * Added UI Tests - broken as is - read desc
    
    - NEED TO EDIT DOES NOT WORK AS IS
    - Added test for checking detailed view of mood post shows correctly
    - Added test for adding a mood post with both min and max amount of things filled out
    - Added test for editing a mood post by getting rid of and adding all things possible
    - Added test for deleting a mood post
    - Need to edit the setup() and seedDatabase() for the tests based on how the database is implemented
    - Need to edit the id for the button on adding a mood post based on how MainActivity was implemented
    
    * Added proper references & updating dataList
    
    - Updated some names of ids and use of functions to match how they were defined in other files
    - Added block of code in HomepageFragment to update dataList whenever the database is updated while the app is running
    
    * Added config for testing
    
    - Added the thing from Lab 7 instructions for proper emulating of a database for testing
    
    * Fixed Returning from posting/editing mood
    
    When returning from posting/editing a mood, PostMoodFragment now properly calls the function to bring us back to the homepage
    
    * dataList fetches all posts
    
    fixed the initializing of dataList only getting posts from current user instead of all users

---
+++ code/app/src/main/java/com/github/bytebandits/bithub/HomepageFragment.java
@@ -0,0 +1,73 @@
+package com.github.bytebandits.bithub;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+public class HomepageFragment extends Fragment implements DatabaseManager.OnPostsFetchListener{
+    private ArrayList<MoodPost> dataList;
+    private ListView moodPostList;
+    private MoodPostArrayAdapter moodPostAdapter;
+
+    @Override
+    public void onPostsFetched(ArrayList<MoodPost> posts) {
+        dataList = posts;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.homepage_fragment, container, false);
+
+        // Initialize dataList from database
+        DatabaseManager.init();
+        DatabaseManager.getPosts(new DatabaseManager.OnPostsFetchListener());
+
+        // Listener so that dataList gets updated whenever the database does
+        CollectionReference moodPostRef = db.getPostsCollectionRef(); // Maybe change later if getter name is wrong
+        moodPostRef.addSnapshotListener((value, error) -> {
+            if (error != null){
+                Log.e("Firestore", error.toString());
+            }
+            if (value != null){
+                dataList.clear();
+                if (!value.isEmpty()) {
+                    for (QueryDocumentSnapshot snapshot : value) {
+                        snapshot.toObject(MoodPost.class);
+                        dataList.add(snapshot.toObject(MoodPost.class));
+                    }
+                }
+                moodPostAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+        // Initialize views and adapters
+        moodPostList = view.findViewById(R.id.homepageMoodPostList);
+        moodPostAdapter = new MoodPostArrayAdapter(getContext(), dataList);
+        moodPostList.setAdapter(moodPostAdapter);
+
+        // on item click on list, open detailed view of post
+        moodPostList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                DetailedMoodPostFragment detailedMoodPostFragment =
+                        DetailedMoodPostFragment.newInstance(dataList.get(position));
+                detailedMoodPostFragment.show(getActivity().getSupportFragmentManager(), "Detailed Mood Post View");
+            }
+        });
+        return view;
+    }
+}
---
+++ code/app/src/main/java/com/github/bytebandits/bithub/MoodPostArrayAdapter.java
@@ -0,0 +1,51 @@
+package com.github.bytebandits.bithub;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class MoodPostArrayAdapter extends ArrayAdapter<MoodPost> {
+    public MoodPostArrayAdapter(Context context, ArrayList<MoodPost> moodPosts) {
+        super(context, 0, moodPosts);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup
+            parent) {
+        // Check if there is a view in convertView we can reuse, if not create a new view
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.mood_post_content,
+                    parent, false);
+        } else {
+            view = convertView;
+        }
+
+        // Get the movie object wanted and text views of the view
+        MoodPost moodPost = getItem(position);
+        ImageView logoView = view.findViewById(R.id.moodIcon);
+        TextView nameView = view.findViewById(R.id.textUserName);
+        TextView dateView = view.findViewById(R.id.textDate);
+        TextView timeView = view.findViewById(R.id.textTime);
+        TextView emotionView = view.findViewById(R.id.textEmotion);
+
+        // Set the text views of the view based on the movie object
+        nameView.setText(moodPost.getUsername());
+        dateView.setText(moodPost.getFormattedPostedDate());
+        timeView.setText(moodPost.getFormattedPostedTime());
+        emotionView.setText(moodPost.getEmotion().getState());
+        logoView.setImageResource(moodPost.getEmotion().getLogoID());
+
+        return view;
+    }
+}
