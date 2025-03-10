9e979e2d3fd73998cb5d9b0cb4540f88c9f1453c
tony-m-yang <145091491+tony-m-yang@users.noreply.github.com>
2025-03-09 23:12:09 -0600

    Ty/moods - Homepage, Create Post Page, and Mood Post implementation (#80)
    * Added needed enumerations and mood logos
    
     - Added Emotions.java which is a enumeration that contains all the moods possible fore a mood post
    - Added SocialSituation.java which is a enumeration that contains all the possible social situations for a mood post
    - Added each mood logo as a xml file under res/drawable
    
    * Added MoodPost Class
    
    - Haven't added the profile variable yet and it's corresponding spot in the constructor and it's setters and getters
    - Haven't fully implemented the getImage() method
    
    * Added viewing of moods and changed moodpost class
    
    - Created the homepage fragment which currently just displays some initialized data
    - Created adapter for mood posts
    - Changed the representation of images in the moodPost class from a String of a imageURL to just a byte array
    - Added fonts and colours for the project
    
    * Ability to click on mood posts for detailed view
    
    - Added detailedViewFragment
    - Added arrows on top of mood posts to indicate the ability to press them
    - Added method in MoodPost class to return formatted date and time for display
    
    * Added ability to post moods
    
    - Added post mood fragment
    - Adjusted Homepage Fragment to call the post mood fragment on a button press (THIS NEEDS TO BE REMOVED LATER ONCE NAV BAR IS IMPLIMENTED, THIS IS JUST TEMP FOR TESTING)
    
    * Added check for valid desc input
    
    - just edited postmoodfragment slightly so that if the user inputs a description that is more than 20 characters or 3 words for their mood post, they'll get a error and the mood post won't be added
    
    * Edited MoodPost to fit UML
    
    Edited the moodPost class to fit the new UML diagram where image is kept as a BitMap and we don't pass location to the MoodPost but a boolean of whether we want a mood post or not and handle the getting of location internally
    
    * Added ability to edit mood posts
    
    - Edited PostMoodFragment so that if you call a new instance of it with the tag "edit mood post" and pass a mood post to it, you can edit a mood post
    - Edited the setter for location in MoodPost class so that its two methods for enabling and disabling location instead of passing a boolean
    - Added background to the spinners and textbox of the post mood fragment xml so its easier to see the text
    
    * Edited for Profile, Database and delete
    
    - THIS VERSION WILL NOT RUN
    - MoodPost is now had a Profile variable instead of a String variable representing the user
    - Everything is now linked to database class rather than storing locally
    - Got rid of all the features that were put purely for testing
    - Added some javadocs to PostMoodFragment methods
    - Added delete mood post function
    
    * Added UI Tests - broken as is - read desc
    
    - NEED TO EDIT DOES NOT WORK AS IS
    - Added test for checking detailed view of mood post shows correctly
    - Added test for adding a mood post with both min and max amount of things filled out
    - Added test for editing a mood post by getting rid of and adding all things possible
    - Added test for deleting a mood post
    - Need to edit the setup() and seedDatabase() for the tests based on how the database is implemented
    - Need to edit the id for the button on adding a mood post based on how MainActivity was implemented
    
    * Added proper references & updating dataList
    
    - Updated some names of ids and use of functions to match how they were defined in other files
    - Added block of code in HomepageFragment to update dataList whenever the database is updated while the app is running
    
    * Added config for testing
    
    - Added the thing from Lab 7 instructions for proper emulating of a database for testing
    
    * Fixed Returning from posting/editing mood
    
    When returning from posting/editing a mood, PostMoodFragment now properly calls the function to bring us back to the homepage
    
    * dataList fetches all posts
    
    fixed the initializing of dataList only getting posts from current user instead of all users

---
+++ code/app/src/main/java/com/github/bytebandits/bithub/Emotion.java
@@ -0,0 +1,63 @@
+package com.github.bytebandits.bithub;
+
+/**
+ * This is a enumeration that represents each possible emotion for a mood post
+ * @author Tony Yang
+ */
+
+public enum Emotion {
+    ANGER("Anger", "intense feeling of displeasure", R.drawable.anger_logo),
+    CONFUSION("Confusion", "state of being uncertain or baffled", R.drawable.confusion_logo),
+    DISGUST("Disgust", "feeling of revulsion and or disapproval", R.drawable.disgust_logo),
+    FEAR("Fear", "  anxiousness from a perceived threat", R.drawable.fear_logo),
+    HAPPINESS("Happiness", "feeling of pleasure or contentment", R.drawable.happiness_logo),
+    SADNESS("Sadness", "feeling or showing of sorrow", R.drawable.sadness_logo),
+    SHAME("Shame", "feeling of humiliation and guilt", R.drawable.shame_logo),
+    SURPRISE("Surprise", "state of shock from a sudden event", R.drawable.surprise_logo);
+
+    private String state;
+    private String description;
+    private int logoID; // ID to reference the mood logo
+
+    /**
+     * Enumerator constructor to initialize the different emotions
+     * @param state
+     *      String representing the emotion's name
+     * @param description
+     *      String representing a short description describing the emotion (max 40 char.)
+     * @param logoID
+     *      Int representing the ID to use to reference the emotion's logo
+     */
+    Emotion(String state, String description, int logoID) {
+        this.state = state;
+        this.description = description;
+        this.logoID = logoID;
+    }
+
+    /**
+     * Returns the name of the emotion
+     * @return
+     *      Returns a string representing the emotion's name
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * Returns the description of the emotion
+     * @return
+     *      Returns a string representing a short description describing the emotion
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the ID of the emotion's logo
+     * @return
+     *      Returns a int representing the ID to use to reference the emotion's logo
+     */
+    public int getLogoID() {
+        return logoID;
+    }
+}
---
+++ code/app/src/main/java/com/github/bytebandits/bithub/MoodPost.java
@@ -0,0 +1,297 @@
+package com.github.bytebandits.bithub;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * This is a class that represents a mood post
+ * @author Tony Yang
+ */
+
+public class MoodPost implements Serializable {
+    private UUID postID;
+    private Emotion emotion;
+    private Profile profile;
+    private Date dateTime;
+    private Location location;
+    private SocialSituation situation;
+    private String desc;
+    private Bitmap image;
+
+    /**
+     * Constructor to make a mood post
+     * @param emotion
+     *      Emotion object representing the emotion of the mood post.
+     *      Cannot be null.
+     * @param profile
+     *      Profile object representing the profile of the user who posted the mood post
+     *      Cannot be null.
+     * @param showLocation
+     *      boolean representing whether or not we should attach our current location to the post
+     * @param situation
+     *      SocialSituation object representing the social situation of the mood post.
+     *      When null is passed, it means that no social situation is attached to the mood post.
+     * @param desc
+     *      String object representing a short description of the mood post.
+     *      Can be a max of 20 characters or 3 words.
+     *      When null is passed, it means that no description is attached to the mood post.
+     * @param image
+     *      A bitmap representing the image attached to the mood post
+     *      When null is passed, it means that no image is attached to the mood post.
+     */
+    public MoodPost(Emotion emotion, Profile profile, boolean showLocation, SocialSituation situation,
+                    String desc, Bitmap image) {
+        this.postID = UUID.randomUUID();
+        this.emotion = emotion;
+        this.profile = profile;
+        this.dateTime = new Date();
+        this.location = null; // change later to save location based on showLocation
+        this.situation = situation;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    /**
+     * Returns the mood post's ID
+     * @return
+     *      Returns a UUID representing the mood post's ID.
+     */
+    public UUID getPostID() {
+        return postID;
+    }
+
+    /**
+     * Returns the mood post's ID as a string for database storage
+     * @return
+     *      Returns a String object representing the mood post's ID
+     */
+    public String getPostIDString() {
+        return getPostID().toString();
+    }
+
+    /**
+     * Returns the emotion of the mood post
+     * @return
+     *      Returns a Emotion object representing the emotion of the mood post.
+     */
+    public Emotion getEmotion() {
+        return emotion;
+    }
+
+    /**
+     * Sets the mood post's emotion
+     * @param emotion
+     *      Emotion object representing the emotion of the mood post.
+     */
+    public void setEmotion(Emotion emotion) {
+        this.emotion = emotion;
+    }
+
+    /**
+     * Returns the mood post's emotion's name as a string for database storage and display
+     * @return
+     *      Returns a String object representing the mood post's emotion's name (ex. "Sadness")
+     */
+    public String getEmotionString() {
+        return getEmotion().getState();
+    }
+
+    /**
+     * Returns the profile of the mood post
+     * @return
+     *      Returns a Profile object representing the profile of the user who posted the mood post
+     */
+    public Profile getProfile() {
+        return profile;
+    }
+
+    /**
+     * Sets the mood post's profile
+     * @param profile
+     *      Profile object representing the profile of the user who posted the mood post
+     */
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    /**
+     * Returns the mood post's profile's UserID/Username for database storage and display
+     * @return
+     *      Returns a String object representing the mood post's profile's username
+     */
+    public String getUsername() {
+        return getProfile().getUserID();
+    }
+
+    /**
+     * Returns the mood post's date and time posted
+     * @return
+     *      Returns a Date object representing the date and time the mood post was posted.
+     */
+    public Date getPostedDateTime() {
+        return dateTime;
+    }
+
+    /**
+     * Sets the mood post's date and time posted
+     * @param dateTime
+     *      Date object representing the date and time the mood post was posted.
+     */
+    public void setPostedDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    /**
+     * Returns the mood post's date and time posted as a string for database storage
+     * @return
+     *      Returns a String object representing the mood post's date and time posted
+     */
+    public String getPostedDateTimeString() {
+        // Format the string before returning
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        return formatter.format(getPostedDateTime());
+    }
+
+    /**
+     * Returns the mood post's date in a formatted string
+     * @return
+     *      Returns a formatted String object representing the mood post's date posted
+     */
+    public String getFormattedPostedDate() {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM. dd, yyyy");
+        return dateFormatter.format(getPostedDateTime());
+    }
+
+    /**
+     * Returns the mood post's time in a formatted string
+     * @return
+     *      Returns a formatted String object representing the mood post's time posted
+     */
+    public String getFormattedPostedTime() {
+        SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mma");
+        return timeFormatter.format(getPostedDateTime());
+    }
+
+    /**
+     * Returns the mood post's attached location
+     * @return
+     *      Returns a Location object representing the mood post's attached location.
+     *      Returns null when the mood post has no attached location.
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets the mood post's attached location
+     */
+    public void showLocation() {
+        this.location = null;  // change later to save location
+    }
+
+    /**
+     * Disables the mood post's attached location
+     */
+    public void hideLocation() {
+        this.location = null;  // change later
+    }
+
+    /**
+     * Returns the mood post's attached location as a string for database storage and display
+     * @return
+     *      Returns a String object representing the mood post's attached location.
+     *      Returns null when the mood post has no attached location.
+     */
+    public String getLocationString() {
+        if (getLocation() == null) { return null; }
+        return getLocation().getLatitude() + "," + getLocation().getLongitude();
+    }
+
+    /**
+     * Returns the mood post's social situation
+     * @return
+     *      Returns a SocialSituation object representing the mood post's social situation.
+     *      Returns null when the mood post has no attached social situation.
+     */
+    public SocialSituation getSocialSituation() {
+        return situation;
+    }
+
+    /**
+     * Sets the mood post's social situation
+     * @param situation
+     *      SocialSituation object representing the mood post's social situation.
+     */
+    public void setSocialSituation(SocialSituation situation) {
+        this.situation = situation;
+    }
+
+    /**
+     * Returns the mood post's social situation's name as a string for database storage and display
+     * @return
+     *      Returns a String object representing the mood post's social situation's name (ex. "ALONE")
+     *      Returns null when the mood post has no attached social situation.
+     */
+    public String getSocialSituationString() {
+        if (getSocialSituation() == null) { return null; }
+        return getSocialSituation().name();
+    }
+
+    /**
+     * Returns the mood post's attached description
+     * @return
+     *      Returns a String object representing a short description of the mood post.
+     *      Returns null when the mood post has no attached description.
+     */
+    public String getDescription() {
+        return desc;
+    }
+
+    /**
+     * Sets the mood post's description
+     * @param desc
+     *      String object representing a short description of the mood post.
+     *      Can be a max of 20 characters or 3 words.
+     */
+    public void setDescription(String desc) {
+        this.desc = desc;
+    }
+
+    /**
+     * Returns the mood post's attached image
+     *
+     * @return
+     *      Returns a bitmap representing the image attached to the mood post
+     *      Returns null when the mood post has no attached image.
+     */
+    public Bitmap getImage() {
+        return image;
+    }
+
+    /**
+     * Sets the mood post's attached image
+     * @param image
+     *      bitmap representing the image attached to the mood post
+     */
+    public void setMoodImage(Bitmap image) {
+        this.image = image;
+    }
+
+    /**
+     * Returns the mood post's attached image as a string for database storage
+     *
+     * @return
+     *      Returns a Base64 String representing the image
+     *      Returns null when the mood post has no attached image.
+     */
+    public String getImageString() {
+        // Check if there is an attached image
+        if (getImage() == null) { return null; }
+        return null; // TODO
+    }
+}
