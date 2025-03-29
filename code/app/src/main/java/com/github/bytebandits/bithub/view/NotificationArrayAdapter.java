04956a139e54f6cdcbadd3afbb3044197d641bc0
SohamLimbachia <90707024+SohamLimbachia@users.noreply.github.com>
2025-03-29 00:01:46 -0600

    Notification model added

--- code/app/src/main/java/com/github/bytebandits/bithub/view/NotificationArrayAdapter.java
+++ code/app/src/main/java/com/github/bytebandits/bithub/view/NotificationArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,11 +14,12 @@ import androidx.core.content.ContextCompat;
 
 import com.github.bytebandits.bithub.R;
 import com.github.bytebandits.bithub.model.MoodPost;
+import com.github.bytebandits.bithub.model.Notification;
 
 import java.util.ArrayList;
 
-public class NotificationArrayAdapter extends ArrayAdapter<MoodPost> {
-    public NotificationArrayAdapter(Context context, ArrayList<MoodPost> moodPosts) {
+public class NotificationArrayAdapter extends ArrayAdapter<Notification> {
+    public NotificationArrayAdapter(Context context, ArrayList<Notification> moodPosts) {
         super(context, 0, moodPosts);
     }
 
@@ -34,19 +36,34 @@ public class NotificationArrayAdapter extends ArrayAdapter<MoodPost> {
             view = convertView;
         }
 
-        // Get the movie object wanted and text views of the view
-        MoodPost moodPost = getItem(position);
+        Notification notification = getItem(position);
         TextView nameView = view.findViewById(R.id.textUserName);
         TextView dateView = view.findViewById(R.id.textDate);
         TextView timeView = view.findViewById(R.id.textTime);
         TextView emotionView = view.findViewById(R.id.emotion_group);
+        ImageButton buttonA = view.findViewById(R.id.accept);
+        ImageButton buttonD = view.findViewById(R.id.decline);
+        TextView textA = view.findViewById(R.id.textTime2);
+        TextView textD = view.findViewById(R.id.textTime5);
 
         // Set the text views of the view based on the movie object
-        nameView.setText(moodPost.getProfile().getUserId());
-        dateView.setText(moodPost.getFormattedPostedDate());
-        timeView.setText(moodPost.getFormattedPostedTime());
-        emotionView.setText(moodPost.getEmotion().getState());
-        emotionView.setTextColor(ContextCompat.getColor(getContext(), moodPost.getEmotion().getColor()));
+        if (notification.getNotificationType() == 1){
+            buttonA.setVisibility(View.VISIBLE);
+            buttonD.setVisibility(View.VISIBLE);
+            textD.setVisibility(View.VISIBLE);
+            textA.setVisibility(View.VISIBLE);
+        }
+        else {
+            buttonA.setVisibility(View.GONE);
+            buttonD.setVisibility(View.GONE);
+            textD.setVisibility(View.GONE);
+            textA.setVisibility(View.GONE);
+        }
+        nameView.setText(notification.getProfile().getUserId());
+        dateView.setText(notification.getFormattedPostedDate());
+        timeView.setText(notification.getFormattedPostedTime());
+        emotionView.setText(notification.getPost().getEmotion().getState());
+        emotionView.setTextColor(ContextCompat.getColor(getContext(), notification.getPost().getEmotion().getColor()));
 
         return view;
     }
